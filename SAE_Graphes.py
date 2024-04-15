7deb154948edbda6978ded54a2c64ad8265534c7
Mouadzouadi <143809277+Mouadzouadi@users.noreply.github.com>
2024-04-15 15:12:41 +0200

    6.2 (#3)
    * 6.2 collaborateurs et modification du 6.1 avec une seule liste pour tous les collaborateurs.
    
    * 6.2 collaborateurs et modification du 6.1 ou touts les collaborateurs sont reunit dans une seule liste

--- SAE_Graphes.py
+++ SAE_Graphes.py
@@ -7,17 +7,17 @@ def txt_json(fichier):
     for ligne in lignes:
         data = json.loads(ligne)
         modif = {}
+        liste = []
         for titre, valeur in data.items():
-            liste = []
             if type(valeur) == list :
                 for carac in valeur:
                     carac = carac.strip('"').replace("[[", "")
                     carac =carac.strip('"').replace("]]", "")
                     liste.append(carac)
-                modif[titre] = liste
+                modif["collaborateurs"] = liste
             if type(valeur) == str:
                 modif[titre] = valeur
-            films.append(modif)
+        films.append(modif)
     with open("data.json", "w") as f:
         json.dump(films,f,indent=4,ensure_ascii=False)
 
@@ -26,3 +26,19 @@ def txt_json(fichier):
      
 txt_json("data_2.txt")
 
+
+def collaborateurs(acteurs1, acteurs2):
+    with open("data.json", "r") as f:
+        data = json.load(f)
+    ens = set()
+    for film in data:
+        for acteurs in film.values():
+            for acteur in acteurs:
+                if acteurs1 == acteur or acteurs2 == acteur :
+                    ens.update(acteurs)
+
+    return ens
+
+
+print(collaborateurs("Anna Lizaran","Harrison Ford")) 
+    
