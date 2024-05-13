b3bcc8265fd97971b23c85709130331b32185987
Mouadzouadi <143809277+Mouadzouadi@users.noreply.github.com>
2024-05-13 11:18:06 +0200

    Update SAE_Graphes.py

--- SAE_Graphes.py
+++ SAE_Graphes.py
@@ -1,4 +1,9 @@
+
 import json
+import networkx as nx
+import matplotlib.pyplot as plt
+
+
 def txt_json(fichier):
     fic = open(fichier, 'r',encoding='utf8')
     fic_json = open("data.json",'w')
@@ -7,22 +12,36 @@ def txt_json(fichier):
     for ligne in lignes:
         data = json.loads(ligne)
         modif = {}
+        liste = []
         for titre, valeur in data.items():
-            liste = []
             if type(valeur) == list :
                 for carac in valeur:
                     carac = carac.strip('"').replace("[[", "")
                     carac =carac.strip('"').replace("]]", "")
                     liste.append(carac)
-                modif[titre] = liste
+                modif["collaborateurs"] = liste
             if type(valeur) == str:
                 modif[titre] = valeur
-            films.append(modif)
+        films.append(modif)
     with open("data.json", "w") as f:
         json.dump(films,f,indent=4,ensure_ascii=False)
 
-    
 
      
-txt_json("data_2.txt")
+#txt_json("data_100.txt")
 
+def json_vers_nx(chemin):
+    G = nx.Graph()
+    with open(chemin, "r") as f:
+        data = json.load(f)
+        for film in data:
+            acteurs = film.get("collaborateurs", [])
+            for i in range(len(acteurs)):
+                for j in range(i+1, len(acteurs)):
+                    G.add_edge(acteurs[i], acteurs[j])
+    return G
+
+G = json_vers_nx("data.json")
+#plt.clf()
+#nx.draw(G)
+#plt.show()
