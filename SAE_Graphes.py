fadd771c6eee3d2ac17423cfdb20855dee5f747e
Mouadzouadi <143809277+Mouadzouadi@users.noreply.github.com>
2024-05-28 15:24:28 +0200

    6.4 (#12)
    * Update SAE_Graphes.py
    
    * Update SAE_Graphes.py
    
    distance et centralite
    
    * Update SAE_Graphes.py
    
    centralite et centre d'holywood

--- SAE_Graphes.py
+++ SAE_Graphes.py
@@ -42,10 +42,12 @@ def json_vers_nx(chemin):
     return G
 
 
+
 G = json_vers_nx("data.json")
 plt.clf()
 nx.draw(G)
 plt.show()
+
 
                 
 
@@ -61,6 +63,7 @@ def collaborateurs_communs(acteurs1, acteurs2):
                     ens.update(acteurs)
 
     return ens
+
 
 
 
@@ -84,7 +87,6 @@ def collaborateurs_proches(G,u,k):
 
     for i in range(k):
         collaborateurs_directs = set()
-
         for c in collaborateurs: 
             for voisin in G.adj[c]:
                 if voisin not in collaborateurs:
@@ -148,10 +150,10 @@ def distance_naive(G, u, v):
     
     return None
 
-ens = collaborateurs_proches(G,"Alexy wiciak",2)
+#ens = collaborateurs_proches(G,"Harrison Ford",2)
 
 
-def est_proche(G, u, v,k):
+def est_proche(G, u, v,k=1):
     """Fonction renvoyant True si u et v sont reliés dans le graphe G, False sinon.
     
     Parametres:
@@ -179,4 +181,128 @@ def distance(G,u,v):
         return nx.shortest_path_length(G,u,v) # Utilisation de la fonction de NetworkX pour calculer la distance
     except nx.NetworkXNoPath: # Si aucun chemin n'existe entre u et v
         return None
+
+
+#print(est_proche(G,"John Cazale","Harrison Ford"))
+
+
+
+def distance_naive(G, u, v):
+    """ Fonction renvoyant la distance entre u et v dans le graphe G. La fonction renvoie None si u ou v sont absents du graphe.
+
+    Args:
+        G (Graphe): le graphe
+        u (String): le sommet de départ, un acteur
+        v (String): le sommet d'arrivée, un acteur
+
+    Returns:
+        int: la distance entre u et v dans le graphe G. La fonction renvoie None si u ou v sont absents du graphe.
+
+    Complexité: O(n+m) où n est le nombre de sommets et m le nombre d'arêtes du graphe.
+    """
+    if u not in G.nodes:
+        return None
+    if v not in G.nodes:
+        return None
+    
+    collaborateurs = set()
+    collaborateurs.add(u)
+    n = 1  # Initialisation de la distance à 1
+    
+    while collaborateurs: # Boucle tant qu'il y a des collaborateurs à examiner
+        collaborateurs_directs = set() # Ensemble pour stocker les collaborateurs directs au prochain niveau
+        for c in collaborateurs:
+            for voisin in G.adj[c]:
+                if voisin == v: # Si le voisin est v, retourne la distance actuelle
+                    return n 
+                if voisin not in collaborateurs:
+                    collaborateurs_directs.add(voisin) # Si le voisin n'est pas déjà un collaborateur, l'ajoute à l'ensemble des collaborateurs directs
+        
+        if not collaborateurs_directs:    # Si aucun nouveau collaborateur n'est trouvé, v n'est pas accessible depuis u
+            return None
+        collaborateurs = collaborateurs_directs # Met à jour les collaborateurs pour le prochain niveau de recherche
+        n += 1
+    
+    return None
+
+
+def distance(G,u,v):
+    """Fonction renvoyant la distance entre u et v dans le graphe G. La fonction renvoie None si u ou v sont absents du graphe.
+    
+    Parametres:
+        G: le graphe
+        u: un sommet
+        v: un sommet
+
+    Complexité: O(n+m) où n est le nombre de sommets et m le nombre d'arêtes du graphe.
+    """
+    if u not in G.nodes:
+        return None
+    if v not in G.nodes:
+        return None
+    try:
+        return nx.shortest_path_length(G,u,v) # Utilisation de la fonction de NetworkX pour calculer la distance
+    except nx.NetworkXNoPath: # Si aucun chemin n'existe entre u et v
+        return None
+
+def distance(G,u,v):
+    """Fonction renvoyant la distance entre u et v dans le graphe G. La fonction renvoie None si u ou v sont absents du graphe.
+    
+    Parametres:
+        G: le graphe
+        u: un sommet
+        v: un sommet
+
+    Complexité: O(n+e) où n est le nombre de sommets et E le nombre d'arêtes du graphe.
+    """
+    if u not in G.nodes:
+        return None
+    if v not in G.nodes:
+        return None
+    try:
+        return nx.shortest_path_length(G,u,v) # Utilisation de la fonction de NetworkX pour calculer la distance
+    except nx.NetworkXNoPath: # Si aucun chemin n'existe entre u et v
+        return None
+
+
+
+def centralite(G, u):
+    """Fonction renvoyant la centralité de l'acteur u dans le graphe G par rapport à la plus grande distance qui le sépare d'un autre acteur. La fonction renvoie None si u est absent du graphe.
+
+    Args:
+        G (Graphe): le graphe
+        u (String): le sommet, un acteur
+
+    Returns:
+        int: la centralité de l'acteur u dans le graphe G. La fonction renvoie None si u est absent du graphe.
+
+    Complexité: O(N(N+E)) où N est le nombre de sommets et E le nombre d'arêtes du graphe.
+
+    """
+    if u not in G.nodes:
+        return None
+    centralite = 0
+    for voisin in G.nodes:
+        if voisin != u:
+            d = distance(G,u,voisin)
+            if d is not None and d > centralite:
+                centralite = d
+    return centralite
+
+#print(centralite(G,"Jennifer Salt"))
+
+    
+def centre_holywood(G):
+    """Fonction renvoyant l'acteur le plus central du graphe G par rapport au minimum de centralite par acteur .
+    
+    Parametres:
+        G: le graphe
+    Returns:
+        str: l'acteur le plus central du graphe G
+    Complexité: O(N(N+E)) où N est le nombre de sommets et E le nombre d'arêtes du graphe.
+    """
+    centralites = {acteur: centralite(G,acteur) for acteur in G.nodes}
+    return min(centralites, key=centralites.get)
+
+#print(centre_holywood(G)) 
 
