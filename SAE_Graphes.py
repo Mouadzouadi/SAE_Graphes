24dd140a40d3914ceaeb56242412518fbe982a80
Mouadzouadi <143809277+Mouadzouadi@users.noreply.github.com>
2024-05-28 15:04:25 +0200

    6.3 (#9)
    * Update SAE_Graphes.py
    
    * Update SAE_Graphes.py
    
    distance_naive et distance

--- SAE_Graphes.py
+++ SAE_Graphes.py
@@ -28,7 +28,7 @@ def txt_json(fichier):
 
 
      
-txt_json("data_100.txt")
+#txt_json("data_100.txt")
 
 def json_vers_nx(chemin):
     G = nx.Graph()
@@ -41,11 +41,14 @@ def json_vers_nx(chemin):
                     G.add_edge(acteurs[i], acteurs[j])
     return G
 
+
 G = json_vers_nx("data.json")
 plt.clf()
 nx.draw(G)
 plt.show()
+
                 
+
 
 def collaborateurs_communs(acteurs1, acteurs2):
     with open("data.json", "r") as f:
@@ -60,7 +63,9 @@ def collaborateurs_communs(acteurs1, acteurs2):
     return ens
 
 
+
 print(collaborateurs_communs("Anna Lizaran","Harrison Ford")) 
+
     
 
 def collaborateurs_proches(G,u,k):
@@ -79,12 +84,69 @@ def collaborateurs_proches(G,u,k):
 
     for i in range(k):
         collaborateurs_directs = set()
-        for c in collaborateurs:
+
+        for c in collaborateurs: 
             for voisin in G.adj[c]:
                 if voisin not in collaborateurs:
                     collaborateurs_directs.add(voisin)
         collaborateurs = collaborateurs.union(collaborateurs_directs)
     return collaborateurs
+  
+#ens = collaborateurs_proches(G,"Harrison Ford",2)
+
+
+def est_proche(G, u, v,k=1):
+    """Fonction renvoyant True si u et v sont reliés dans le graphe G, False sinon.
+    
+    Parametres:
+        G: le graphe
+        u: un sommet
+        v: un sommet
+    """
+    return u in collaborateurs_proches(G,v,k)
+
+
+#print(est_proche(G,"John Cazale","Harrison Ford"))
+
+
+
+def distance_naive(G, u, v):
+    """ Fonction renvoyant la distance entre u et v dans le graphe G. La fonction renvoie None si u ou v sont absents du graphe.
+
+    Args:
+        G (Graphe): le graphe
+        u (String): le sommet de départ, un acteur
+        v (String): le sommet d'arrivée, un acteur
+
+    Returns:
+        int: la distance entre u et v dans le graphe G. La fonction renvoie None si u ou v sont absents du graphe.
+
+    Complexité: O(n+m) où n est le nombre de sommets et m le nombre d'arêtes du graphe.
+    """
+    if u not in G.nodes:
+        return None
+    if v not in G.nodes:
+        return None
+    
+    collaborateurs = set()
+    collaborateurs.add(u)
+    n = 1  # Initialisation de la distance à 1
+    
+    while collaborateurs: # Boucle tant qu'il y a des collaborateurs à examiner
+        collaborateurs_directs = set() # Ensemble pour stocker les collaborateurs directs au prochain niveau
+        for c in collaborateurs:
+            for voisin in G.adj[c]:
+                if voisin == v: # Si le voisin est v, retourne la distance actuelle
+                    return n 
+                if voisin not in collaborateurs:
+                    collaborateurs_directs.add(voisin) # Si le voisin n'est pas déjà un collaborateur, l'ajoute à l'ensemble des collaborateurs directs
+        
+        if not collaborateurs_directs:    # Si aucun nouveau collaborateur n'est trouvé, v n'est pas accessible depuis u
+            return None
+        collaborateurs = collaborateurs_directs # Met à jour les collaborateurs pour le prochain niveau de recherche
+        n += 1
+    
+    return None
 
 ens = collaborateurs_proches(G,"Alexy wiciak",2)
 
@@ -99,5 +161,22 @@ def est_proche(G, u, v,k):
     """
     return u in collaborateurs_proches(G,v,k)
 
+def distance(G,u,v):
+    """Fonction renvoyant la distance entre u et v dans le graphe G. La fonction renvoie None si u ou v sont absents du graphe.
+    
+    Parametres:
+        G: le graphe
+        u: un sommet
+        v: un sommet
 
-print(est_proche(G,"John Cazale","Harrison Ford"))
+    Complexité: O(n+m) où n est le nombre de sommets et m le nombre d'arêtes du graphe.
+    """
+    if u not in G.nodes:
+        return None
+    if v not in G.nodes:
+        return None
+    try:
+        return nx.shortest_path_length(G,u,v) # Utilisation de la fonction de NetworkX pour calculer la distance
+    except nx.NetworkXNoPath: # Si aucun chemin n'existe entre u et v
+        return None
+
